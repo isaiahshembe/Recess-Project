b95b5963496e0ddb4f558630973703ed3d6687fe
ofwonojohn <ofwonojohn004@gmail.com>
2024-07-07 07:15:45 +0300

    created page for editing profile page

---
+++ touristapp/lib/pages/editprofilepage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () async {},
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
