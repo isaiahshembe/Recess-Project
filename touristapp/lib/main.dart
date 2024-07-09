fd9f2adf350c0d55ac8cf15e0dade5cf00cb0366
ofwonojohn <ofwonojohn004@gmail.com>
2024-07-09 15:11:28 +0300

    added some theme functions

--- touristapp/lib/main.dart
+++ touristapp/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:touristapp/localization/app_localizations.dart';
+import 'package:touristapp/pages/settings/localprovider.dart';
+import 'package:touristapp/pages/settings/themeprovider.dart';
 import 'package:touristapp/pages/splash%20screen/splashscreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => LocaleProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 
@@ -12,8 +25,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final localeProvider = Provider.of<LocaleProvider>(context);
+    
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: themeProvider.isDarkMode ? Brightness.dark : Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      locale: localeProvider.locale,
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('es', 'ES'),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       home: SplashScreen(),
 
     );
