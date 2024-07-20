61775d5e76646f6846d9ec0c56f8955211ebd1ee
ofwonojohn <ofwonojohn004@gmail.com>
2024-07-20 21:36:08 +0300

    added firebase initialization

--- touristapp/lib/main.dart
+++ touristapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get/get.dart';
+import 'package:touristapp/firebase_options.dart';
 import 'package:touristapp/pages/settings/localprovider.dart';
 import 'package:touristapp/pages/settings/themeprovider.dart';
 import 'package:touristapp/pages/splash%20screen/splashscreen.dart';
@@ -11,7 +12,9 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
