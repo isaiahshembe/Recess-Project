abb4976414395a1ed8eaaee36e64dce5e905c399
ofwonojohn <ofwonojohn004@gmail.com>
2024-07-24 16:00:02 +0300

    modified edit page to update a user's details

--- touristapp/lib/pages/profile/editprofilepage.dart
+++ touristapp/lib/pages/profile/editprofilepage.dart
@@ -1,225 +1,121 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
+import 'dart:io';
 
 class ProfileEditScreen extends StatefulWidget {
-  const ProfileEditScreen({super.key});
-
-  static const routeName = '/profile-edit-screen';
-
   @override
-  State<ProfileEditScreen> createState() => _ProfileEditScreenState();
+  _ProfileEditScreenState createState() => _ProfileEditScreenState();
 }
 
 class _ProfileEditScreenState extends State<ProfileEditScreen> {
-  late TextEditingController nameController;
-  late TextEditingController emailController;
-  late TextEditingController addressController;
-  late String phoneNumber;
-  late String countryCode;
-
-  bool _isUpdating = false; // Flag to track the update process
-
-  var isLoading = false;
-  var isobx = false;
-
+  User? user;
+  TextEditingController? _nameController;
+  TextEditingController? _emailController;
   File? _image;
-  final _picker = ImagePicker();
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    nameController = TextEditingController();
-    emailController = TextEditingController();
-    addressController = TextEditingController();
-
-    // Dummy data for testing
-    nameController.text = "";
-    emailController.text = "";
-    phoneNumber = "";
-    countryCode = "";
-    addressController.text = "";
+    user = FirebaseAuth.instance.currentUser;
+    _nameController = TextEditingController(text: user?.displayName);
+    _emailController = TextEditingController(text: user?.email);
   }
 
-  @override
-  void dispose() {
-    nameController.dispose();
-    emailController.dispose();
-    addressController.dispose();
-    super.dispose();
-  }
-
-  Future<void> getImageGally() async {
-    final pickedFile = await _picker.pickImage(
-      source: ImageSource.gallery,
-      imageQuality: 80,
-    );
-
-    setState(() {
-      if (pickedFile != null) {
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
         _image = File(pickedFile.path);
-      } else {
-        print("No Image Picked");
-      }
-    });
+      });
+    }
   }
 
-  Widget showUserData() {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          formField(
-            icon: Icons.person,
-            controller: nameController,
-            inputType: TextInputType.name,
-            hint: "name",
-          ),
-          const SizedBox(height: 20.0),
-          formField(
-            icon: Icons.email,
-            controller: emailController,
-            inputType: TextInputType.emailAddress,
-            hint: "email",
-          ),
-          const SizedBox(height: 20.0),
-          phoneField(),
-          const SizedBox(height: 20.0),
-          formField(
-            icon: Icons.location_city_rounded,
-            controller: addressController,
-            inputType: TextInputType.text,
-            hint: "address",
-          ),
-          const SizedBox(height: 25.0),
-          InkWell(
-            onTap: () {
-              getImageGally();
-            },
-            child: Container(
-              height: 150.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.black),
-              ),
-              child: _image != null
-                  ? Image.file(_image!)
-                  : Image.asset("images/display_image1.jpg"),
-            ),
-          ),
-          const SizedBox(height: 25.0),
-          InkWell(
-            onTap: _isUpdating
-                ? null
-                : () async {
-                    setState(() {
-                      _isUpdating = true;
-                    });
+  Future<void> _uploadImageAndSaveProfile() async {
+    if (_image != null) {
+      setState(() {
+        _isLoading = true;
+      });
+      try {
+        // Upload image to Firebase Storage
+        String fileName = 'profile_pictures/${user!.uid}.jpg';
+        UploadTask uploadTask = FirebaseStorage.instance.ref(fileName).putFile(_image!);
+        TaskSnapshot snapshot = await uploadTask;
+        String downloadURL = await snapshot.ref.getDownloadURL();
 
-                    // Simulate a delay to mimic an update process
-                    await Future.delayed(const Duration(seconds: 2));
+        // Update Firestore user document
+        await FirebaseFirestore.instance.collection('users').doc(user!.uid).set({
+          'displayName': _nameController!.text,
+          'photoURL': downloadURL,
+        }, SetOptions(merge: true));
 
-                    try {
-                      Fluttertoast.showToast(
-                        msg: "Updated Successfully",
-                        backgroundColor: Colors.black87,
-                      );
-                      Get.back();
-                    } catch (e) {
-                      Fluttertoast.showToast(
-                        msg: "Something went wrong",
-                        backgroundColor: Colors.black87,
-                      );
-                    } finally {
-                      setState(() {
-                        _isUpdating = false;
-                      });
-                    }
-                  },
-            child: Container(
-              height: 60.0,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                gradient: LinearGradient(
-                  colors: [Colors.blue, Colors.purple],
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                ),
-              ),
-              child: _isUpdating
-                  ? const SizedBox(
-                      width: 20,
-                      height: 20,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                      ),
-                    )
-                  : Center(
-                      child: Text(
-                        "Update",
-                        style: GoogleFonts.courgette(
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+        // Update FirebaseAuth user profile
+        await user!.updateProfile(displayName: _nameController!.text, photoURL: downloadURL);
 
-  Widget phoneField() {
-    return IntlPhoneField(
-      initialCountryCode: 'UG',
-      onChanged: (phone) {
+        // Show success message
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Profile updated successfully')));
+      } catch (e) {
+        // Show error message
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to update profile: $e')));
+      } finally {
         setState(() {
-          countryCode = phone.countryCode;
-          phoneNumber = phone.number;
+          _isLoading = false;
         });
-      },
-      decoration: const InputDecoration(
-        labelText: 'Phone Number',
-        border: OutlineInputBorder(
-          borderSide: BorderSide(),
-        ),
-      ),
-    );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('No image selected')));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile Edit".tr),
+        title: Text('Edit Profile'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _uploadImageAndSaveProfile,
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: showUserData(),
-      ),
-    );
-  }
-
-  Widget formField({
-    required IconData icon,
-    required TextEditingController controller,
-    required TextInputType inputType,
-    required String hint,
-    bool readOnly = false,
-  }) {
-    return TextFormField(
-      controller: controller,
-      keyboardType: inputType,
-      readOnly: readOnly,
-      decoration: InputDecoration(
-        prefixIcon: Icon(icon),
-        hintText: hint,
-        border: const OutlineInputBorder(),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: _pickImage,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: _image != null
+                          ? FileImage(_image!)
+                          : (user!.photoURL != null
+                              ? NetworkImage(user!.photoURL!)
+                              : AssetImage('assets/images/default_profile_image.jpg')) as ImageProvider,
+                      child: _image == null
+                          ? Icon(Icons.camera_alt, size: 50, color: Colors.white70)
+                          : null,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    readOnly: true, // Email is not editable
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
