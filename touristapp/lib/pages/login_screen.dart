3ebcfa26e6b1ac9fa31e32ed503d9df54f431df7
ofwonojohn <ofwonojohn004@gmail.com>
2024-07-24 15:59:35 +0300

    added function to capture a user details and saved to firebase

--- touristapp/lib/pages/login_screen.dart
+++ touristapp/lib/pages/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:touristapp/pages/Welcomepage/welcomepage.dart'; 
+import 'package:touristapp/pages/Welcomepage/features/features.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> _signInWithEmailAndPassword() async {
     try {
@@ -22,10 +24,16 @@ class _LoginScreenState extends State<LoginScreen> {
         email: _emailController.text,
         password: _passwordController.text,
       );
+      User? user = userCredential.user;
+
+      if (user != null) {
+        await _createOrUpdateUserDocument(user);
+      }
+
       // Navigate to HomePage on successful login
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const WelcomePage()),
+        MaterialPageRoute(builder: (context) => const TrendingFeaturesPage()),
       );
     } catch (e) {
       // Handle login error
@@ -44,10 +52,16 @@ class _LoginScreenState extends State<LoginScreen> {
         idToken: googleAuth.idToken,
       );
       UserCredential userCredential = await _auth.signInWithCredential(credential);
+      User? user = userCredential.user;
+
+      if (user != null) {
+        await _createOrUpdateUserDocument(user);
+      }
+
       // Navigate to HomePage on successful login
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const WelcomePage()),
+        MaterialPageRoute(builder: (context) => const TrendingFeaturesPage()),
       );
     } catch (e) {
       // Handle login error
@@ -57,47 +71,68 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  Future<void> _createOrUpdateUserDocument(User user) async {
+    DocumentSnapshot doc = await _firestore.collection('users').doc(user.uid).get();
+    if (!doc.exists) {
+      // Create a new user document if it doesn't exist
+      await _firestore.collection('users').doc(user.uid).set({
+        'displayName': user.displayName ?? '',
+        'email': user.email ?? '',
+        'phoneNumber': user.phoneNumber ?? '',
+        'photoURL': user.photoURL ?? '',
+      });
+    } else {
+      // Optionally, update the existing document with any new information
+      await _firestore.collection('users').doc(user.uid).update({
+        'displayName': user.displayName ?? '',
+        'email': user.email ?? '',
+        'phoneNumber': user.phoneNumber ?? '',
+        'photoURL': user.photoURL ?? '',
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       
+        title: const Text('Login'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child:Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: _signInWithEmailAndPassword,
-              icon: const Icon(Icons.email),
-              label: const Text('Login with Email'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: _signInWithGoogle,
-              icon: Image.asset(
-                'images/icons8-google-48.png', // Path to your Google icon
-                height: 24.0, // Adjust the size if necessary
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
               ),
-              label: const Text('Continue with Google'),
-            ),
-            const SizedBox(height: 20),
-          ],
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: _signInWithEmailAndPassword,
+                icon: const Icon(Icons.email),
+                label: const Text('Login with Email'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: _signInWithGoogle,
+                icon: Image.asset(
+                  'images/icons8-google-48.png', // Path to your Google icon
+                  height: 24.0, // Adjust the size if necessary
+                ),
+                label: const Text('Continue with Google'),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
